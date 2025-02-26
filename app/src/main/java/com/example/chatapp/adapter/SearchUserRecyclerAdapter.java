351f67ceaf913d5bb6297774262c37a7972bf0b8
anbeoo1112 <lean1112work1@gmail.com>
2025-02-26 21:51:39 +0700

    update Search User Activity

---
+++ app/src/main/java/com/example/chatapp/adapter/SearchUserRecyclerAdapter.java
@@ -0,0 +1,49 @@
+package com.example.chatapp.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.chatapp.R;
+import com.example.chatapp.model.UserModel;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
+public class SearchUserRecyclerAdapter extends FirestoreRecyclerAdapter<UserModel, SearchUserRecyclerAdapter.UserModelViewHolder> {
+
+    public SearchUserRecyclerAdapter(@NonNull FirestoreRecyclerOptions<UserModel> options, Context applicationContext) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull SearchUserRecyclerAdapter.UserModelViewHolder holder, int position, @NonNull UserModel model) {
+        holder.usernameText.setText(model.getUsername());
+        holder.phoneText.setText(model.getPhone());
+
+    }
+
+    @NonNull
+    @Override
+    public SearchUserRecyclerAdapter.UserModelViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    class UserModelViewHolder extends RecyclerView.ViewHolder {
+        TextView usernameText, phoneText;
+        ImageView profileImage;
+
+        public UserModelViewHolder(@NonNull View itemView) {
+            super(itemView);
+            usernameText = itemView.findViewById(R.id.search_user_input);
+            phoneText = itemView.findViewById(R.id.phone_text);
+            profileImage = itemView.findViewById(R.id.profile_pic);
+        }
+    }
+}
+
+
