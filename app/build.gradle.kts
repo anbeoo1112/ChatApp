25fb64f71e166b225a8d12781858d8dec814f5ac
anbeoo1112 <lean1112work1@gmail.com>
2025-02-26 16:50:29 +0700

    update searchactivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.firebaseui:firebase-ui-firestore:8.0.2")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+
 
 }
--- app/src/main/java/com/example/chatapp/SearchUserActivity.java
+++ app/src/main/java/com/example/chatapp/SearchUserActivity.java
@@ -1,14 +1,29 @@
 package com.example.chatapp;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.chatapp.adapter.SearchUserRecyclerAdapter;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
 
 public class SearchUserActivity extends AppCompatActivity {
+    private ImageButton searchButton,backButton;
+    private EditText searchInput;
+    RecyclerView recyclerView;
+
+    SearchUserRecyclerAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,60 @@ public class SearchUserActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        searchInput = findViewById(R.id.search_user_input);
+        searchButton = findViewById(R.id.search_user_btn);
+        backButton = findViewById(R.id.back_btn);
+        recyclerView = findViewById(R.id.search_user_recycler_view);
+        searchInput.requestFocus();
+
+        backButton.setOnClickListener(v -> {
+            onBackPressed();
+        });
+
+        searchButton.setOnClickListener(v -> {
+            String searchTerm = searchInput.getText().toString();
+            if(searchTerm.isEmpty() || searchTerm.length()<3) {
+                searchInput.setError("Please enter a username");
+                return;
+            }
+            setupSearchRecyclerView(searchTerm);
+        });
+    }
+
+    void setupSearchRecyclerView(String searchTerm) {
+
+        Query query = FirebaseFirestore.getInstance().collection("users")
+                .whereGreaterThanOrEqualTo("username", searchTerm)
+                .whereLessThanOrEqualTo("username", searchTerm + "\uf8ff");
+        adapter = new SearchUserRecyclerAdapter(searchTerm,getApplicationContext());
+        recyclerView.setAdapter(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(adapter != null) {
+            adapter.startListening();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(adapter != null) {
+            adapter.stopListening();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(adapter != null) {
+            adapter.startListening();
+        }
     }
 }
