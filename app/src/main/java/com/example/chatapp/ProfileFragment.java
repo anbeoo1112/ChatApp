80712ec038f938291f81a50b6699b65137db655a
anbeoo1112 <lean1112work1@gmail.com>
2025-03-01 14:57:01 +0700

    Profile: update profile and logout

--- app/src/main/java/com/example/chatapp/ProfileFragment.java
+++ app/src/main/java/com/example/chatapp/ProfileFragment.java
@@ -1,28 +1,105 @@
 package com.example.chatapp;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.example.chatapp.model.UserModel;
+import com.example.chatapp.utils.FirebaseUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.firestore.DocumentSnapshot;
 
 public class ProfileFragment extends Fragment {
+
+    private ImageView profilePic;
+    private EditText username, phone;
+    private Button updateProfileBtn;
+    private ProgressBar progressBar;
+    private TextView logoutBtn;
+
+    private UserModel currentUserModel;
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-
-
-
-
+    @SuppressLint("MissingInflatedId")
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        profilePic = view.findViewById(R.id.profile_image_view);
+        phone = view.findViewById(R.id.profile_phone);
+        username = view.findViewById(R.id.profile_username);
+        progressBar = view.findViewById(R.id.profile_progress_bar);
+        logoutBtn = view.findViewById(R.id.logout_btn);
+        updateProfileBtn = view.findViewById(R.id.profile_update_btn); // Khởi tạo nút cập nhật
+
+        getUserData();
+
+        updateProfileBtn.setOnClickListener(v -> updateUserProfile());
+        logoutBtn.setOnClickListener(v-> {
+            FirebaseUtil.logOut();
+            Intent intent = new Intent(getContext(), SplashActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        });
+        return view;
+    }
+
+    private void getUserData() {
+        setInProgress(true);
+        FirebaseUtil.currentUserDetails().get().addOnCompleteListener(task -> {
+            setInProgress(false);
+            if (task.isSuccessful() && task.getResult() != null) {
+                currentUserModel = task.getResult().toObject(UserModel.class);
+                if (currentUserModel != null) {
+                    username.setText(currentUserModel.getUsername());
+                    phone.setText(currentUserModel.getPhone());
+                }
+            } else {
+                Toast.makeText(getContext(), "Failed to load user data", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void updateUserProfile() {
+        if (currentUserModel == null) return;
+        setInProgress(true);
+        currentUserModel.setUsername(username.getText().toString());
+        currentUserModel.setPhone(phone.getText().toString());
+
+        FirebaseUtil.currentUserDetails().set(currentUserModel).addOnCompleteListener(task -> {
+            setInProgress(false);
+            if (task.isSuccessful()) {
+                Toast.makeText(getContext(), "Profile updated successfully", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getContext(), "Profile update failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+
+    private void setInProgress(boolean inProgress) {
+        if (inProgress) {
+            progressBar.setVisibility(View.VISIBLE);
+            updateProfileBtn.setVisibility(View.GONE);
+        } else {
+            progressBar.setVisibility(View.GONE);
+            updateProfileBtn.setVisibility(View.VISIBLE);
+        }
     }
 }
